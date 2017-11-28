b8698de60376d221857de5cdb9e99804eb30dd48
weisico <mt3530@163.com>
2017-11-28 08:58:27 +0800

    update

--- app/src/main/java/com/weisico/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/weisico/myapplication/DisplayMessageActivity.java
@@ -1,11 +1,14 @@
 package com.weisico.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -24,6 +27,15 @@ public class DisplayMessageActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
     }
 
 }
